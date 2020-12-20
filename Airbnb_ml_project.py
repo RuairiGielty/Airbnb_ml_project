2063a6e7f7fc956b7357db335dba278d76e1c89f
Ruairí Gielty <gieltyr@tcd.ie>
2020-12-20 19:44:46 +0000

    Update Airbnb_ml_project.py

--- Airbnb_ml_project.py
+++ Airbnb_ml_project.py
@@ -26,7 +26,7 @@ def setClassValues(price, ratings):
             y[i] = -1
     return y
 
-def setNaN(ratings):
+def set_review_scores_NaN(ratings):
     booleanRatings = np.isnan(ratings)
     print(booleanRatings)
     for i in range(len(booleanRatings)):
@@ -47,14 +47,24 @@ def main():
     #prices column to numpy array
     prices_pd = pd.to_numeric(dublin_listings['price'], errors='coerce')
     prices = prices_pd.to_numpy()
+    np.reshape(prices,(-1,1))
+    prices = prices[:,np.newaxis]
 
     #host_is_superhost string column to numpy array of bools
     is_superhost = dublin_listings['host_is_superhost'].to_numpy()
     is_superhost[is_superhost == 't'] = 1
     is_superhost[is_superhost == 'f'] = 0
+    np.reshape(is_superhost,(-1,1))
+    is_superhost = is_superhost[:,np.newaxis]
 
     #review_scores_rating to numpy array
     review_scores = dublin_listings['review_scores_rating'].to_numpy()
+    np.reshape(review_scores,(-1,1))
+    review_scores = review_scores[:,np.newaxis]
+    #print((review_scores == np.nan))
+    #review_scores1 = set_review_scores_NaN(review_scores)
+    #ind = np.where(review_scores1 == 74.36976803)
+    #print(np.array_equal(review_scores[ind],review_scores1[ind]))
 
     #amenities to numpy array storing number of amenities for each listing
     amenities_pd = dublin_listings['amenities']
@@ -63,22 +73,9 @@ def main():
     for x in range(len(amenities_pd)):
         amenities[x] = len(np.array((amenities_pd[x].replace('[','').replace(']','')).split(',')))
     amenities.astype(int)
-
-    X = np.column_stack((prices, review_scores))
-
-    print(prices.shape)
-    print(amenities.shape)
-    print(is_superhost.shape)
-    print(review_scores.shape)
-
-    review_scores = setNaN(review_scores)
-    y = setClassValues(review_scores, prices)
-    print(y)
-
-    review_scores = review_scores.reshape(-1,1)
-    model = LogisticRegression(penalty = 'none', solver='lbfgs').fit(review_scores, prices)
-    ypred = model.predict(review_scores)
-    print(ypred)
+    #reshape to be compatible with sklearn functions
+    np.reshape(amenities,(-1,1))
+    amenities = amenities[:,np.newaxis]
 
     plt.rc('font', size=14)
     plt.rcParams['figure.constrained_layout.use'] = True
